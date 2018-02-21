94e19663079568e7e479b374607a931058444b99
Ivan Ivaleev <ivaleev1997@gmail.com>
2018-02-21 22:41:30 +0300

    Added permission checker and requester

--- app/src/main/java/com/example/arteme/myapplication/MainActivity.java
+++ app/src/main/java/com/example/arteme/myapplication/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.arteme.myapplication;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity {
+    private final static int INTERNET_PERMISSION_REQUEST = 8435;
+    private final static int ACCESS_FINE_LOCATION_PERMISSION_REQUEST = 8434;
+    private final static int ACCESS_NETWORK_STATE_PERMISSION_REQUEST = 8436;
 
     @Override
     protected void onResume() {
@@ -17,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        permissionChecker();
     }
 
     public void sendComOrd(View view) {
@@ -35,4 +43,39 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    private void permissionChecker() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.INTERNET},
+                    INTERNET_PERMISSION_REQUEST);
+        }
+
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    ACCESS_FINE_LOCATION_PERMISSION_REQUEST);
+        }
+
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_NETWORK_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.ACCESS_NETWORK_STATE},
+                    ACCESS_NETWORK_STATE_PERMISSION_REQUEST);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
 }
